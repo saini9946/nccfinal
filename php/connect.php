2eda455bd2204d465c5ae543d8dcaf355d6e3ad6
pmsndsh <33567882+pmsndsh@users.noreply.github.com>
2018-11-16 11:51:43 +0530

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "localhost";
-$username = "root";
+$username = "";
 $password = "";
-$conn = new mysqli($servername, $username, $password,'ncc');
+$conn = new mysqli($servername, $username, $password,'database_name');
 ?>
